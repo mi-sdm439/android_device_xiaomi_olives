1250863480918bbd4c4fbdf0eee3483efb218417
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-08-06 13:10:11 +0800

    RevengeOS: olives: Create revengeos makefile

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -11,3 +11,11 @@ COMMON_LUNCH_CHOICES := \
     lineage_olives-user \
     lineage_olives-userdebug \
     lineage_olives-eng
+
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/revengeos_olives.mk
+
+COMMON_LUNCH_CHOICES += \
+    revengeos_olives-user \
+    revengeos_olives-userdebug \
+    revengeos_olives-eng
