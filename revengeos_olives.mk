6880323167e823eb104c85af6ff08c54357e5380
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-09-25 10:11:56 +0800

    RevengeOS: olives: Set official status

--- revengeos_olives.mk
+++ revengeos_olives.mk
@@ -31,3 +31,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "Xiaomi/olive/olive:10/QKQ1.191014.001/V12.5.1.0.QCNCNXM:user/release-keys"
+
+# RevengeOS
+REVENGEOS_BUILDTYPE := OFFICIAL
