a2f21cdc70e530d12a2f4a9ef1b1eefd8f722bc4
shield44 <motoecondorxt1022@gmail.com>
2021-10-12 13:45:27 +0530

    olives: audio: Update audiopolicies to R
    * from T89571AA3-user-11-RKQ1.200928.002-00WW_3_150-release-key
    
    Change-Id: Ic45b19bbccc530b3b8d2bb61b6c736a46482f609

--- device.mk
+++ device.mk
@@ -26,8 +26,10 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
-    $(LOCAL_PATH)/audio/mixer_paths_sdm439_pm8953.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_sdm439_pm8953.xml
+    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_mtp.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_sku1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_sku1.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_sdm439_sku1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_sdm439_sku1.xml
 
 # Consumer IR
 PRODUCT_PACKAGES += \
